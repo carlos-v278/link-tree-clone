a82a6962b45b917ed71b1ef77f3443c67a493bbe
carlosvieira student <carlosvieira278@gmail.com>
2023-01-12 17:55:06 +0100

    test fetch current user relation data

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -5,14 +5,21 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\User;
+use Symfony\Component\Security\Core\Security;
+use App\Repository\SocialRepository;
+use App\Repository\UserRepository;
 
 class HomeController extends AbstractController
 {
     #[Route('/home', name: 'app_home')]
-    public function index(): Response
+    public function index(Security $security): Response
     {
+        $user = $this->getUser();
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
+            'currentUser' => $user,
         ]);
     }
 }
--- src/Controller/TestController.php
+++ src/Controller/TestController.php
@@ -2,17 +2,77 @@
 
 namespace App\Controller;
 
+use App\Entity\Ressource;
+use App\Form\RessourceType;
+use App\Repository\RessourceRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+#[Route('/test')]
 class TestController extends AbstractController
 {
-    #[Route('/test', name: 'app_test')]
-    public function index(): Response
+    #[Route('/', name: 'app_test_index', methods: ['GET'])]
+    public function index(RessourceRepository $ressourceRepository): Response
     {
         return $this->render('test/index.html.twig', [
-            'controller_name' => 'TestController',
+            'ressources' => $ressourceRepository->findAll(),
         ]);
+    }
+
+    #[Route('/new', name: 'app_test_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, RessourceRepository $ressourceRepository): Response
+    {
+        $ressource = new Ressource();
+        $form = $this->createForm(RessourceType::class, $ressource);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ressourceRepository->save($ressource, true);
+
+            return $this->redirectToRoute('app_test_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('test/new.html.twig', [
+            'ressource' => $ressource,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_test_show', methods: ['GET'])]
+    public function show(Ressource $ressource): Response
+    {
+        return $this->render('test/show.html.twig', [
+            'ressource' => $ressource,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_test_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Ressource $ressource, RessourceRepository $ressourceRepository): Response
+    {
+        $form = $this->createForm(RessourceType::class, $ressource);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $ressourceRepository->save($ressource, true);
+
+            return $this->redirectToRoute('app_test_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('test/edit.html.twig', [
+            'ressource' => $ressource,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_test_delete', methods: ['POST'])]
+    public function delete(Request $request, Ressource $ressource, RessourceRepository $ressourceRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$ressource->getId(), $request->request->get('_token'))) {
+            $ressourceRepository->remove($ressource, true);
+        }
+
+        return $this->redirectToRoute('app_test_index', [], Response::HTTP_SEE_OTHER);
     }
 }
